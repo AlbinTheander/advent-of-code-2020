5bdebaf714f5df9cb9723fd33444ecc5f0248ba7
Albin Theander <Albin.Theander@jayway.com>
2020-12-01 08:38:15 +0100

    Added typescript crude solution to day 1

---
+++ typescript/src/day01/index.ts
@@ -0,0 +1,31 @@
+import fs from "fs";
+
+export default function day1() {
+  const data = fs.readFileSync("../data/day01.txt", "utf-8");
+  const nums = data.split("\n").map(Number);
+  const prod1 = part1(nums);
+  const prod2 = part2(nums);
+  console.log('====== Day 1 ======')
+  console.log('The product of the first pair is', prod1);
+  console.log('The product of the second triplet is', prod2);
+}
+
+function part1(nums: number[]): number {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] + nums[j] === 2020) return nums[i] * nums[j];
+    }
+  }
+  return -1;
+}
+
+function part2(nums: number[]): number {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      for (let k = 0; k < nums.length; k++) {
+        if (i !== j && i !== k && j !== k && nums[i] + nums[j] + nums[k] === 2020) return nums[i] * nums[j] * nums[k];
+      }
+    }
+  }
+  return -1;
+}
