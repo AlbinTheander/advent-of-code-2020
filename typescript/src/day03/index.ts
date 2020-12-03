236fac8fbf73e8a5f565249648e2017eea277eb5
Albin Theander <Albin.Theander@jayway.com>
2020-12-03 08:29:46 +0100

    Day 3 - Typescript

---
+++ typescript/src/day03/index.ts
@@ -0,0 +1,35 @@
+import fs from 'fs';
+
+export default function day3() {
+  const data = fs.readFileSync("../data/day03.txt", "utf-8");
+  const map = parseData(data);
+  const answer1 = countTrees(map, 3, 1);
+  const answer2 = countManySlopes(map);
+  console.log('====== Day 3 ======')
+  console.log('Barraging down the hill, I encounter', answer1, 'trees');
+  console.log('Trying different slopes, I get the magical number', answer2);
+}
+
+function parseData(s) {
+  return s.split('\n');
+}
+
+function countManySlopes(map: string[]) {
+  return [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    .map(([dx, dy]) => countTrees(map, dx, dy))
+    .reduce((a, b) => a * b, 1);
+}
+
+function countTrees(map: string[], dx: number, dy: number): number {
+  let x = 0;
+  let y = 0;
+  let count = 0;
+
+  while(y < map.length) {
+    if (map[y][x % map[0].length] === '#') count++;
+    y += dy;
+    x += dx;
+  }
+
+  return count;
+}
