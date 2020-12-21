cdd0b616a95f08ce69ff55c08196d682436dd6a4
Albin Theander <Albin.Theander@jayway.com>
2020-12-21 19:10:23 +0100

    Day 13 - cleanup

--- typescript/src/__test__/day13.test.ts
+++ typescript/src/__test__/day13.test.ts
@@ -1,10 +1,11 @@
-import { part2 } from "../day13"
+import { parseData, part2 } from "../day13"
 
+  const testLines = (s: string): number => part2(parseData('0\n' + s).buses)
 test('works', () => {
-  expect(part2('17,x,13,19')).toEqual(3417);
-  expect(part2('67,7,59,61')).toEqual(754018);
-  expect(part2('67,x,7,59,61')).toEqual(779210);
-  expect(part2('1789,37,47,1889')).toEqual(1202161486);
+  expect(testLines('17,x,13,19')).toEqual(3417);
+  expect(testLines('67,7,59,61')).toEqual(754018);
+  expect(testLines('67,x,7,59,61')).toEqual(779210);
+  expect(testLines('1789,37,47,1889')).toEqual(1202161486);
 })
 
 115106 % 67
--- typescript/src/day13.ts
+++ typescript/src/day13.ts
@@ -1,28 +1,39 @@
-export default function day13(data: string) {
-  const lines = data.split('\n');
-  const timestamp = +lines[0];
-  const buses = lines[1].split(',').map(Number).filter(n => !isNaN(n));
+type Bus = { id: number, offset: number };
 
-  const answer = part2(lines[1]);
-  console.log(answer);
-  return;
-  let bus;
-  let wait = 0;
-  while(wait < 500) {
-    bus = buses.find(b => (timestamp + wait) % b === 0);
-    if (bus) break;
-    wait++;
-  }
-  // console.log(buses);
-  console.log(bus, wait, bus * wait);
+export default function day13(data: string) {
+  const { timestamp, buses } = parseData(data);
+
+  const answer1 = part1(timestamp, buses);
+  const answer2 = part2(buses);
+  const { busId, waitTime } = answer1;
+
+  console.log('===== Day 13 =====')
+  console.log('Best bus is id', busId, 'after waiting', waitTime, 'seconds. Magic number is', busId * waitTime);
+  console.log('The first time the buses go in sequence is', answer2);
 }
 
+export function parseData(data: string): { timestamp: number, buses: Bus[] } {
+  const lines = data.split('\n');
+  const timestamp = +lines[0];
+  const buses = lines[1].split(',')
+    .map((s, i) => ({ id: +s, offset: (i % +s) }))
+    .filter(bus => !isNaN(bus.id));
 
-export function part2(busLine: string) {
-  const buses = busLine.split(',')
-    .map((s, i) => ({id: +s, offset: (i % +s)}))
-    .filter(b => !isNaN(b.id))
+  return { timestamp, buses };
+}
 
+function part1(timestamp: number, buses: Bus[]): { busId: number, waitTime: number} {
+  let bus: Bus;
+  let waitTime = 0;
+  while(!bus) {
+    bus = buses.find(b => (timestamp + waitTime) % b.id === 0);
+    waitTime++;
+  }
+  waitTime--;
+  return { busId: bus.id, waitTime }
+}
+
+export function part2(buses: Bus[]) {
   let n = buses[0].offset;
   let mul = buses[0].id;
   for(let bus of buses.slice(1)) {
