521420c3a5ee6a3bef4104b3aa7d58831e4ba57e
Albin Theander <Albin.Theander@jayway.com>
2020-12-30 19:57:59 +0100

    Day 15 cleanup. Still slow

--- typescript/src/day15.ts
+++ typescript/src/day15.ts
@@ -1,27 +1,32 @@
 export default function day15(data: string) {
   const ns = data.split(',').map(Number);
-  console.log(ns);
+  const answer1 = playGame(ns, 2020);
+  const answer2 = playGame(ns, 30000000);
 
+  console.log('===== Day 15 =====');
+  console.log('After 2020 rounds, the current number is', answer1);
+  console.log('After 30000000 rounds, the current number is', answer2);
+}
+
+function playGame(startingNumbers: number[], rounds: number): number {
   const latest = new Map<number, number>();
 
   let turn = 1;
-  let current = ns.pop();
-  while(turn <= ns.length) {
-    latest.set(ns[turn-1], turn);
+  while(turn <= startingNumbers.length-1) {
+    latest.set(startingNumbers[turn-1], turn);
     turn++;
   }
 
-  while(turn < 30000000) {
+  let current = startingNumbers[startingNumbers.length-1];
+  while(turn < rounds) {
     let next = 0;
     if (latest.has(current)) {
       next = turn - latest.get(current);
     }
-    // console.log('I got', current, latest);
-    // console.log('I say', next);
-    if (turn % 1000000 === 0) console.log(turn, current);
     latest.set(current, turn);
     current = next;
     turn++;
   }
-  console.log(current);
+
+  return current;
 }
